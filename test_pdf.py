9174608d927fbbfd591f9a5382b5a2487786a271
Opex <opex618@gmail.com>
2022-01-10 05:17:33 +0700

    git ignore

--- test_pdf.py
+++
@@ -1,4 +0,0 @@
-from app.ballistic.shoot import main as make_sticker
-
-if __name__=='__main__':
-    make_sticker( 'test/table.csv','test/table.htm','test/out.pdf',sticker_len_mm=75,round_to_click=False,click_on_turn=80)
